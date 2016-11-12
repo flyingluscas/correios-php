4f773bad2aa266d94900be5f9b6c1af14323caae
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-11-12 15:55:49 -0200

    added service class

---
+++ src/Service.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FlyingLucas\Correios;
+
+abstract class Service
+{
+    /**
+     * PAC code.
+     */
+    const PAC = 41106;
+
+    /**
+     * Sedex code.
+     */
+    const SEDEX = 40010;
+
+    /**
+     * Sedex a Crobrar code.
+     */
+    const SEDEX_A_COBRAR = 40045;
+
+    /**
+     * Sedex 10 code.
+     */
+    const SEDEX_10 = 40215;
+
+    /**
+     * Sedex Hoje code.
+     */
+    const SEDEX_HOJE= 40290;
+}
