e0904b79b89d8189a56b0767f160943fd5f73418
Lucas Pires <lucas.pires.mattos@gmail.com>
2017-04-16 01:37:31 -0300

    --amend

--- src/PackageType.php
+++ src/PackageType.php
@@ -4,9 +4,18 @@ namespace FlyingLuscas\Correios;
 
 abstract class PackageType
 {
+    /**
+     * Formatos caixa ou pacote.
+     */
     const BOX = 1;
 
+    /**
+     * Formatos rolo ou prisma.
+     */
     const ROLL = 2;
 
+    /**
+     * Formato envelope.
+     */
     const ENVELOPE = 3;
 }
