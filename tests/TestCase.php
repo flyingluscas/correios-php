6eb8797b232af24ccab1af0db277761d63a9287d
Lucas Pires <lucas.pires.mattos@gmail.com>
2018-12-25 23:51:01 -0200

    test-case: show all errors

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,10 @@ use PHPUnit_Framework_TestCase;
 
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
-    //
+    public function setUp()
+    {
+        parent::setUp();
+
+        error_reporting(E_ALL);
+    }
 }
